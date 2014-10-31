b6bb12321066f6759c2f0ce3be0ef04072b419db
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-10-31 13:56:03 +0000

    Wrap factory_girl linter in a transaction.
    Since https://github.com/thoughtbot/factory_girl/pull/707 the linter now
    calls create on each factory, and this was polluting the database.
    Wrapping this in a transaction that's rolled back prevents this.

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,6 +2,9 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
-    FactoryGirl.lint
+    ActiveRecord::Base.transaction do
+      FactoryGirl.lint
+      raise ActiveRecord::Rollback.new
+    end
   end
 end
