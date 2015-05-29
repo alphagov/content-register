76055bd328861a2eaf1c6f889c18ca0b91d82fd4
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-05-29 17:28:45 +0100

    Verify pacts during CI build
    using the captured build artefact

--- jenkins.sh
+++ jenkins.sh
@@ -7,3 +7,4 @@ git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:drop db:create db:schema:load
 bundle exec rake ci:setup:rspec default
+bundle exec rake pact:verify:at[https://ci-new.alphagov.co.uk/job/govuk_gds_api_adapters_pact_contract_tests/lastSuccessfulBuild/artifact/spec/pacts/gds_api_adapters-content_register.json]
